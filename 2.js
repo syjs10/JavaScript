04afda6b42526a6efaeb3e49f3cdde6472b81733
syjs10 <350172898@qq.com>
2016-03-25 22:23:58 +0800

    JavaScript

---
+++ 2.js
@@ -0,0 +1,7 @@
+var y = "you";
+var mysay = "I" + "love" + y; // = 后面是串表达式，mysay 值是字符串
+var mynum = 12 + 6 * 2; // = 后面是数值表达式，mynum 值是数值
+var mybool = mynum > 12; // = 后面是布尔表达式，mysay 值是布尔值
+console.log(mysay);
+console.log(mynum);
+console.log(mybool);
---
+++ 3.js
@@ -0,0 +1,8 @@
+var num1, num2, res1, res2;
+num1 = 5;
+num2 = 2;
+res1 = num1 + 30 > 10 && num2 * 3 < 20;
+res2 = ((num1 + 30) / (7 - num2)) * 3;
+var result1 = "res1结果为：" + res1;
+var result2 = "res2结果为：" + res2;
+console.log(result1 + "\n" + result2);
