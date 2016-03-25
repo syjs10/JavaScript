00289db099850aa2a867fbb72031b208ab530cf0
syjs10 <350172898@qq.com>
2016-03-25 23:16:11 +0800

    JavaScript

--- Time.js
+++ Time.js
@@ -8,3 +8,8 @@ var myminutes =now.getMinutes(); // 分钟
 var myseconds = now.getSeconds(); // 秒钟
 console.log("今天是" + myyear + "年"+ mymonth + "月" + mydate + "日");
 console.log("时间是" + myhours + "点"+ myminutes + "分" + myseconds + "秒");
+var now = new Date();
+var weekday=["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"];
+var mynum = now.getDay();
+console.log(mynum);
+console.log(weekday[mynum]);
