7175e6e27562f99ec0ef027c42e9f327c7633e82
JS <350172898@qq.com>
2016-10-11 16:06:46 +0800

    Js

---
+++ newlearn/test.js
@@ -0,0 +1,9 @@
+function num() {
+      var age = document.getElementById("age").value;
+      if (age == "" || isNaN(age)) {
+            window.alert('不是数字');
+
+      }
+      document.write(Date());
+      console.log("hello world");
+}
